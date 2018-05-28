f0ae57eee97467d97cc092d643e9d1104f37d2d9
Destiny Zinke <dzinke@student.neumont.edu>
2018-05-28 15:31:40 -0600

    Base Canvas Created

--- Pizza_Lab/script.js
+++ Pizza_Lab/script.js
@@ -1,12 +1,64 @@
-var canvas  = document.createElement('CANVAS');
-canvas.setAttribute("id", "canvas_pizza");
 
-var cWidth = document.getElementById('canvas_pizza');
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(125, 100, 50, 0, 2 * Math.PI);
-ctx.fillStyle = '#B89557';
-ctx.fill();
-ctx.stroke();
-document.body.appendChild(canvas);
+function createHTML(){
+    var css = "<link rel=stylesheet href=style.css>";
+    document.head.innerHTML += css;
+    createPizzas();
+}
+
+
+function createPizzas(){
+    var pizza = document.createElement("DIV");
+    pizza.innerHTML = "<canvas id=small_pizza><button></button></canvas><canvas id=medium_pizza><button></button></canvas><canvas id=large_pizza><button></button></canvas>"
+    pizza.width = 1000;
+    pizza.height = 1000;
+    
+    document.body.appendChild(pizza);
+    pizza.setAttribute("id", "pizzaList")   
+    createSmallPizza();
+    createMedPizza()
+    createLargePizza();
+}
+
+
+function createSmallPizza(){
+    var spCanvas = document.getElementById('small_pizza');
+    var spCtx = spCanvas.getContext("2d");
+    
+    spCanvas.width = 250;
+    spCanvas.height = 300;
+    spCtx.beginPath();
+    spCtx.arc(125, 100, 75,0, 2*Math.PI);
+    spCtx.fillStyle = '#D4BB75';
+    spCtx.fill();
+    document.body.appendChild(spCanvas);
+}
+
+function createMedPizza(){
+    var mpCanvas = document.getElementById('medium_pizza');
+    var mpCtx = mpCanvas.getContext("2d");
+    
+    mpCanvas.width = 250;
+    mpCanvas.height = 300;
+    mpCtx.beginPath();
+    mpCtx.arc(150, 125, 100,0, 2*Math.PI);
+    mpCtx.fillStyle = '#D4BB75';
+    mpCtx.fill();
+    document.body.appendChild(mpCanvas);
+}
+
+function createLargePizza(){
+    var lpCanvas = document.getElementById('large_pizza');
+    var lpCtx = lpCanvas.getContext("2d");
+    
+    lpCanvas.width = 350;
+    lpCanvas.height = 300;
+    lpCtx.beginPath();
+    lpCtx.arc(175, 150, 125,0, 2*Math.PI);
+    lpCtx.fillStyle = '#D4BB75';
+    lpCtx.fill();
+    document.body.appendChild(lpCanvas);
+}
+
+createHTML();
+
 
