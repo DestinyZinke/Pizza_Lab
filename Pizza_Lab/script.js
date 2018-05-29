49d93dbe82c917b96f6a8b07b6ab0ab482883670
Destiny Zinke <dzinke@student.neumont.edu>
2018-05-29 12:15:53 -0600

    Topping Buttons
    Added topping Buttons

--- Pizza_Lab/script.js
+++ Pizza_Lab/script.js
@@ -1,3 +1,7 @@
+var buttonNames = ['Pepperoni', 'Bacon', 'Grilled Chicken', 'Olives', 'Pineapple', 'Canadian Bacon', 'Anchovies', 'Peppers', 'Onions', 'Extra Cheese'];
+var buttons = [];
+
+
 
 function createHTML(){
     var reset = "<link rel=stylesheet href=reset.css>"
@@ -11,25 +15,33 @@ function navBar(){
     var nav = document.createElement("NAV");
     nav.setAttribute("id","navBar"); 
     nav.innerHTML += "<ul id=navList><li id=navCompany class=navItem><a href=index.html>" +
-    "Pizza Now</a></li><li class=navItem><a href=index.html>Home</a></li><li class=navItem><a href=default.html>Order</a></li>" +
-    "<li class=navItem><a href=contact.html>Contact Us</a></li></ul>"
+    "Pizza Now</a></li><li class=navItem><a href=index.html>Home</a></li><li class=navItem><a href=default.html>Order</a></li><li class=navItem><a href=contact.html>Contact Us</a></li></ul>"
     document.body.appendChild(nav);
 }
 
 
 function createPizzas(){
     var pizza = document.createElement("DIV");
-    pizza.innerHTML = "<canvas id=small_pizza><button></button></canvas><canvas id=medium_pizza><button></button></canvas><canvas id=large_pizza><button></button></canvas>"
+    pizza.innerHTML = "<canvas id=small_pizza></canvas><canvas id=medium_pizza></canvas><canvas id=large_pizza></canvas>" +
+    "<canvas id=extra_large_pizza></canvas>"
     pizza.width = 1000;
     pizza.height = 1000;
     
     document.body.appendChild(pizza);
     pizza.setAttribute("id", "pizzaList")   
+
+
     createSmallPizza();
     createMedPizza()
     createLargePizza();
+    createExtraLargePizza();
 }
 
+function createPizzaSizes(){
+    var small_choice = document.createElement("DIV");
+        small_choice.innerHTML = "Small Pizza";
+        document.body.appendChild('small');
+}
 
 function createSmallPizza(){
     var spCanvas = document.getElementById('small_pizza');
@@ -38,10 +50,12 @@ function createSmallPizza(){
     spCanvas.width = 250;
     spCanvas.height = 300;
     spCtx.beginPath();
-    spCtx.arc(125, 100, 75,0, 2*Math.PI);
+    spCtx.arc(100, 75, 50,0, 2*Math.PI);
     spCtx.fillStyle = '#D4BB75';
     spCtx.fill();
     document.body.appendChild(spCanvas);
+
+    
 }
 
 function createMedPizza(){
@@ -51,7 +65,7 @@ function createMedPizza(){
     mpCanvas.width = 250;
     mpCanvas.height = 300;
     mpCtx.beginPath();
-    mpCtx.arc(150, 125, 100,0, 2*Math.PI);
+    mpCtx.arc(125, 100, 75,0, 2*Math.PI);
     mpCtx.fillStyle = '#D4BB75';
     mpCtx.fill();
     document.body.appendChild(mpCanvas);
@@ -64,11 +78,81 @@ function createLargePizza(){
     lpCanvas.width = 350;
     lpCanvas.height = 300;
     lpCtx.beginPath();
-    lpCtx.arc(175, 150, 125,0, 2*Math.PI);
+    lpCtx.arc(150, 125, 100,0, 2*Math.PI);
     lpCtx.fillStyle = '#D4BB75';
     lpCtx.fill();
     document.body.appendChild(lpCanvas);
 }
+
+function createExtraLargePizza(){
+    var elpCanvas = document.getElementById('extra_large_pizza');
+    var elpCtx = elpCanvas.getContext("2d");
+    
+    elpCanvas.width = 350;
+    elpCanvas.height = 300;
+    elpCtx.beginPath();
+    elpCtx.arc(175, 150, 125,0, 2*Math.PI);
+    elpCtx.fillStyle = '#D4BB75';
+    elpCtx.fill();
+    document.body.appendChild(elpCanvas);
+}
+
+function currentPizza(){
+    var pizza = document.createElement("DIV");
+    pizza.innerHTML = "<canvas id=current_pizza><button></button></canvas>"
+    pizza.width = 1000;
+    pizza.height = 1000;
+    
+    document.body.appendChild(pizza);
+    pizza.setAttribute("id", "currentPizza")   
+
+    var canvas = document.getElementById('current_pizza');
+    var ctx = canvas.getContext("2d");
+    
+    canvas.width = 250;
+    canvas.height = 300;
+    ctx.beginPath();
+    ctx.arc(150, 125, 100,0, 2*Math.PI);
+    ctx.fillStyle = '#D4BB75';
+    ctx.fill();
+    document.body.appendChild(canvas);
+
+    createToppingContainer();
+    createButtonContainer();
+
+    
+}
+
+function createToppingContainer(){
+    var tText = document.createElement('DIV');
+    tText.innerHTML = "Current Toppings";
+    document.body.appendChild(tText);
+    tText.setAttribute('id', 'topping_list'); 
+}
+
+function createButtonContainer(){
+    var container = document.getElementById('container');
+    var buttons = document.createElement('DIV'); 
+    buttonNames.forEach(buildButton);
+  }
+  
+  function buildButton(item, index, arr){
+
+    
+    console.log("button " + item + " at index " + index + ' created.');
+    buttons[index] = document.createElement('div');
+    buttons[index].textContent = item;
+    buttons[index].setAttribute('class', 'btn');
+    container.appendChild(buttons[index]);
+    buttons[index].addEventListener('click', btnClicked);
+  }
+  
+  function btnClicked(evt) {
+    console.log(evt.target.innerText + " clicked");
+    var temp = evt;
+    document.getElementById('topping_list') += temp;
+    console.log(document.getElementById('topping_list').innerHTML);
+  }
 
 createHTML();
 
